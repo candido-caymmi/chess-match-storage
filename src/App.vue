f2cf7d0633ef8ec080e716e3d02b9670fb010d22
candido-caymmi <118845949+Fanjasma@users.noreply.github.com>
2023-12-29 20:24:49 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+    <nav class="navbar navbar-expand bg-body-tertiary p-3">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <button class="btn me-2" type="submit">Sign In</button>
+                        <button class="btn btn-primary" type="submit">Sign Up</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
