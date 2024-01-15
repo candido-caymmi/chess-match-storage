9ac61fdbf0e94d6d0556fece4a894541639eebb6
candido-caymmi <118845949+Fanjasma@users.noreply.github.com>
2024-01-15 09:58:46 -0300

    Serve Vue frontend through Node with Express

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { connectDB } from './db.js';
 
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const app = express();
 
 // Middleware
+app.use('/static', express.static(path.join(dirname, 'public')));
 app.use(cors());
 app.use(bodyParser.json());
 
 // DB Connection
 connectDB();
 
-// Routes
+//Routes
 import matches from './routes/api/matches.js';
 
 app.get('/', (req, res) => {
-    res.send('Node.js server with Express running!');
+    res.sendFile(path.join(dirname, 'public', 'index.html'));
 });
 
 app.use('/matches', matches);
